e53af094ec7af60d3b6b939a7ac546b1c4b955f5
TC <teresa.j.campbell@gmail.com>
2022-02-19 09:34:06 -0500

    added bcrypt

--- models/User.js
+++ models/User.js
@@ -2,6 +2,7 @@
 //imported Model class and DataTypes object from Sequelize
 const {Model, DataTypes} = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 //create our User model
 class User extends Model {}
@@ -47,6 +48,20 @@ User.init(
     }
   },
   {
+    hooks: {
+      // set up beforeCreate lifecycle "hook" functionality
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      // set up beforeUpdate lifecycle "hook" functionality
+  async beforeUpdate(updatedUserData) {
+    updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+    return updatedUserData;
+  }
+},
+    },
+    {
     sequelize,
     timestamps: false,
     freezeTableName: true,
